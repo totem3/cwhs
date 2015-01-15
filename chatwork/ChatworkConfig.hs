f3d7d19ef05cd917de567cce3b0ab85df5dc0d31
Takafumi Hirata <takhirata6@gmail.com>
2015-01-16 05:29:15 +0900

    reimplement ChatworkConfig

--- chatwork/ChatworkConfig.hs
+++ chatwork/ChatworkConfig.hs
@@ -4,25 +4,15 @@ import Control.Monad.Trans.Reader
 import Data.List (intercalate)
 
 data ChatworkConfig = ChatworkConfig {
-  apiBase :: Maybe String,
-  office :: Maybe String,
-  user :: String,
-  pass :: String
-} deriving (Show)
+                        base :: String,
+                        office :: String,
+                        user :: String,
+                        pass :: String
+                      }
 
-type Chatwork m = ReaderT ChatworkConfig m
+type Chatwork = ReaderT ChatworkConfig
 
-class MonadChatwork m where
-  getApiBase :: m String
-
-buildUrl :: [String] -> Chatwork m String
+buildUrl :: [String] -> Chatwork IO String
 buildUrl paths = do
-  cw <- ask
-  base <- runReaderT cw
-  return $ base ++ (intercalate "/" paths)
-
-{-
-*Chatwork> let c = ChatworkConfig {apiBase="https://kcw.kddi.ne.jp/",office=Just "foo",user="hoge",pass="fuga"}
-*Chatwork> runReaderT cw c
-"https://kcw.kddi.ne.jp/foo/bar"
--} 
+  _base <- fmap base ask
+  return $ _base ++ (intercalate "/" paths)
