fa4ad5c75e0e11e625a6b85c1a7e1e564c1b246c
Takafumi Hirata <takhirata6@gmail.com>
2015-10-04 21:08:23 +0900

    Use state in request

--- chatwork/V0/Api.hs
+++ chatwork/V0/Api.hs
@@ -35,76 +35,68 @@ type ApiResponse a = Maybe (Chatwork.V0.Type.Response a)
 
 sendChat :: String -> String -> Chatwork IO (ApiResponse ())
 sendChat rid body = do
-  maybeAuth <- fmap auth State.get
-  case maybeAuth of
-    Just auth -> post "send_chat" params postdata auth
-                where
-                  params = []
-                  postdata = SendChatData {
-                               text = body,
-                               room_id = rid,
-                               last_chat_id = "0",
-                               read = "0",
-                               edit_id = "0"
-                             }
-    Nothing -> return Nothing
+  post "send_chat" params postdata
+  where
+    params = []
+    postdata = SendChatData {
+                 text = body,
+                 room_id = rid,
+                 last_chat_id = "0",
+                 read = "0",
+                 edit_id = "0"
+               }
 
 getUpdate :: String -> Chatwork IO (ApiResponse GetUpdate)
 getUpdate _lastId = do
-  auth <- fmap auth State.get
-  case auth of
-    Just a -> get "get_update" params a
-                where params = [ ("last_id" , _lastId),
-                                 ("new"     , "1") ]
-    Nothing -> return Nothing
+  get "get_update" params
+  where params = [ ("last_id" , _lastId),
+                   ("new"     , "1") ]
 
 readChat :: RoomId -> MessageId -> Chatwork IO (ApiResponse ReadChat)
 readChat roomId lastChatId = do
-  auth <- fmap auth State.get
-  case auth of
-    Just a -> get "read" params a
-                where params = [ ("last_chat_id" , lastChatId),
-                                 ("room_id"      , roomId) ]
-    Nothing -> return Nothing
+  get "read" params
+  where params = [ ("last_chat_id" , lastChatId),
+                   ("room_id"      , roomId) ]
 
 loadChat :: RoomId -> Chatwork IO (ApiResponse LoadChat)
 loadChat roomId = do
-  auth <- fmap auth State.get
-  case auth of
-    Just a -> get "load_chat" params a
-                where params = [ ("room_id"         ,  roomId),
-                                 ("last_chat_id"    ,  "0"),
-                                 ("first_chat_id"   ,  "0"),
-                                 ("jump_to_chat_id" ,  "0"),
-                                 ("unread_num"      ,  "0"),
-                                 ("desc"            ,  "1") ]
-    Nothing -> return Nothing
+  get "load_chat" params
+  where params = [ ("room_id"         ,  roomId),
+                   ("last_chat_id"    ,  "0"),
+                   ("first_chat_id"   ,  "0"),
+                   ("jump_to_chat_id" ,  "0"),
+                   ("unread_num"      ,  "0"),
+                   ("desc"            ,  "1") ]
 
-get :: (FromJSON a) => String -> [(String, String)] -> Auth -> Chatwork IO (Maybe (Chatwork.V0.Type.Response a))
-get cmd params auth = request "GET" cmd params Nothing auth
+get :: (FromJSON a) => String -> [(String, String)] -> Chatwork IO (Maybe (Chatwork.V0.Type.Response a))
+get cmd params = request "GET" cmd params Nothing
 
-post :: (FromJSON a, ToJSON b) => String -> [(String, String)] -> b -> Auth -> Chatwork IO (Maybe (Chatwork.V0.Type.Response a))
-post cmd params postdata auth = request "POST" cmd params (Just pdata) auth
+post :: (FromJSON a, ToJSON b) => String -> [(String, String)] -> b -> Chatwork IO (Maybe (Chatwork.V0.Type.Response a))
+post cmd params postdata = request "POST" cmd params (Just pdata)
   where
     pdata = RequestBodyLBS $ "pdata=" `BL.append` json
     json = encode postdata
 
 --                         method    command   query paramter        post data
-request :: (FromJSON a) => Method -> String -> [(String, String)] -> Maybe RequestBody -> Auth -> Chatwork IO (Maybe (Chatwork.V0.Type.Response a))
-request method cmd params postdata auth = do
+request :: (FromJSON a) => Method -> String -> [(String, String)] -> Maybe RequestBody -> Chatwork IO (Maybe (Chatwork.V0.Type.Response a))
+request method cmd params postdata = do
   let time = unsafePerformIO $ formatTime defaultTimeLocale "%s" <$> getCurrentTime
-  let query = foldl (join "&") [] $ map zipTuple (params ++ authParams auth ++ [("_", time)])
-  base <- fmap base State.get
-  let url = base ++ "gateway.php?cmd=" ++ cmd ++ "&" ++ query
-  _req <- parseUrl url
-  let contentType = ("Content-Type", C.pack "application/x-www-form-urlencoded; charset=UTF-8")
-  let req = case postdata of
-              Just m  -> _req {cookieJar = Just (jar auth), method = method, requestBody = m, requestHeaders = [contentType]}
-              Nothing -> _req {cookieJar = Just (jar auth), method = method}
-  manager <- liftIO $ newManager tlsManagerSettings
-  runResourceT $ do
-    res <- httpLbs req manager
-    pure $ decode (responseBody res)
+  maybeAuth <- fmap auth State.get
+  case maybeAuth of
+    Just auth -> do
+      let query = foldl (join "&") [] $ map zipTuple (params ++ authParams auth ++ [("_", time)])
+      base <- fmap base State.get
+      let url = base ++ "gateway.php?cmd=" ++ cmd ++ "&" ++ query
+      _req <- parseUrl url
+      let contentType = ("Content-Type", C.pack "application/x-www-form-urlencoded; charset=UTF-8")
+      let req = case postdata of
+                  Just m  -> _req {cookieJar = Just (jar auth), method = method, requestBody = m, requestHeaders = [contentType]}
+                  Nothing -> _req {cookieJar = Just (jar auth), method = method}
+      manager <- liftIO $ newManager tlsManagerSettings
+      runResourceT $ do
+        res <- httpLbs req manager
+        pure $ decode (responseBody res)
+    Nothing -> return Nothing
   where
     zipTuple (a, b) = a ++ "=" ++ b
     join s a b = a ++ s ++ b
